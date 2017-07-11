ba6e6e14ee000c9084c9b3272eec35b53c603c7d
Kaan <kaankabalak@gmail.com>
2017-07-11 14:19:43 -0700

    initial commit

--- AgingPeople/ViewController.swift
+++ AgingPeople/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var people = ["Kaan", "Jerome", "Aaron",
+                  "Erick", "Coco", "Kris",
+                  "Wenmin", "Sarah", "Heather",
+                  "Lucas", "Mike", "Jesse"]
+    var ages: [Int] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        for _ in 0..<people.count {
+            ages.append(Int(arc4random_uniform(91))+5)
+        }
+        print(ages)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +37,15 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = people[indexPath.row]
+        cell.detailTextLabel?.text = "\(ages[indexPath.row]) years old"
+        return cell
+    }
+}
